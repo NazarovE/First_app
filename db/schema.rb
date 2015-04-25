cb98bb4468973fcc4752024565c28a009b7be108
Radik Safin <safinrraf@ymail.com>
2015-04-25 19:04:33 +0300

    Remeber user part 1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422195319) do
+ActiveRecord::Schema.define(version: 20150425124615) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150422195319) do
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
     t.string   "surname",         limit: 255
+    t.string   "remember_digest", limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
